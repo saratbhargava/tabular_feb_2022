f2b29d011bc4ea05cef7cf48735881e45dfc1f3f
saratbhargava <sarat2895@gmail.com>
2022-02-15 00:02:50 -0800

    Add XGBboost Classifier to model dispatcher

--- src/model_dispatcher.py
+++ src/model_dispatcher.py
@@ -2,9 +2,12 @@ import config
 
 from sklearn import ensemble, tree, svm
 
+from xgboost import XGBClassifier
+
 # Declare the various models
 models = {
     "decision_tree": tree.DecisionTreeClassifier,
     "rf": ensemble.RandomForestClassifier,
+    "xgb": XGBClassifier,
     "svm": svm.LinearSVC,
 }
