62bf35b33e3e2ded22b85454b34d8ac5b6ea2736
saratbhargava <sarat2895@gmail.com>
2022-02-17 22:50:33 -0800

    Add xgb classifer hyperparam search and wandb local mode option

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
+import os
+
 from hyperopt import hp
 from hyperopt.pyll.base import scope
+
 
 DEVICE = "cpu" # "cpu", "cuda"
 
 TRAINING_FILE = "../input/train.csv"
 
 TESTING_FILE = "../input/test.csv"
+
+os.environ['WANDB_MODE'] = 'offline'
 
 MODELS = "../models/"
 
@@ -27,11 +32,20 @@ hyper_params = {
     "rf": {
         "n_estimators": hp.choice("n_estimators", [50, 100, 200, 500, 700, 1000, 1200, 1300, 1500]),
         "criterion": hp.choice("criteria", ["gini", "entropy"]),
+        "n_jobs": -1,
         "min_samples_split": scope.int(hp.quniform("min_samples_split", 2, 10, 1)),
     },
     "svm": {
         "penalty": hp.choice("penalty", ["l1", "l2"]),
-        "loss": hp.choice("loss", ["hinge"]),
-        "C": hp.loguniform("C", [-2, 8]),
+        # "loss": hp.choice("loss", ["hinge"]),
+        "C": hp.loguniform("C", -2, 8),
     },
+    "xgb":{
+        "learning_rate": hp.choice("learning_rate", [0.01, 0.1, 0.3]),
+        "n_estimators": hp.choice("n_estimators", [50, 100]),
+        "max_depth": hp.choice("max_depth", [6, 10, 15]),
+        "reg_lambda": hp.choice("reg_lambda", [0.01, 0.1, 1, 10]),
+        "min_split_loss": hp.choice("min_split_loss", [0, 0.1, 0.2, 0.5]),
+        "n_jobs": -1,
+    }
 }
