414326575f7cbe1e0c2a96da7ae314abb27f0923
saratbhargava <sarat2895@gmail.com>
2022-02-17 22:52:12 -0800

    Complete automated train to kaggle submission script!
    This is a fantastic idea, and great point to be really proud of!
    
    The next step should be to get a sense of xgb classifer and tune it properly:)

--- src/submit.sh
+++ src/submit.sh
@@ -1,4 +1,49 @@
 #/bin/bash
 
+#########  PARAMTERS ######################
+
+# select fold index
+FOLD=2
+
+# select ml model type
+MODEL=xgb
+
+# number of trails
+NUM_TRAILS=1
+
+# Submit message
+SUBMIT_MESSAGE = "XGB first submission using complete pipeline"
+
+#########  TRAIN AND SUBMIT MODEL ###########
+
+# Best Model
+MODEL_FILENAME=${MODEL}_fold${FOLD}_${date+%m_%d_%Y_%H_%M_%S}.bin
+
+SUBMIT_FILENAME=${MODEL_FILENAME:0:-4}.csv
+
+echo Training ${MODEL} model started at `date`
+SECONDS=0
+
+# train the model
+python train.py --fold ${FOLD} --model ${MODEL} --num_trails ${NUM_TRAILS} --model_filename ${MODEL_FILENAME}
+
+train_duration=$SECONDS
+echo Training ${MODEL} model complete at `date`
+echo Training duration: ${train_duration} seconds
+
+# test the model
+echo Inference ${MODEL} model started at `date`
+SECONDS=0
+
+python inference.py --model_filename ${MODEL_FILENAME} --submit_filename ${SUBMIT_FILENAME}
+
+inference_duration=$SECONDS
+echo Inference ${MODEL} model complete at `date`
+echo Inference duration: ${train_duration} seconds
+
 # Submit the output csv to kaggle
-kaggle competitions submit -c tabular-playground-series-feb-2022 -f ../submit/rf_fold1_12_02_2022_19_50_43.csv -m "First submition using a pipeline"
+kaggle competitions submit -c tabular-playground-series-feb-2022 -f ../submit/${SUBMIT_FILENAME} -m ${SUBMIT_MESSAGE}
+
+echo Kaggle submission complete
+
+##############################################
